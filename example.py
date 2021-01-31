671f81fcd402faf2d30ab70cd2282b1d1577bb26
ngron <syumuu1219@outlook.jp>
2021-01-31 21:08:08 +0900

    cronで実行させる

--- example.py
+++ example.py
@@ -1,3 +1,4 @@
+# coding: UTF-8
 import RPi.GPIO as GPIO
 import dht11
 import time
@@ -17,35 +18,37 @@ url = "https://notify-api.line.me/api/notify"
 token = os.environ["LINE_NOTIFY_TOKEN"]
 headers = {"Authorization" : "Bearer "+ token}
 
+MIN_TEMPERATURE = 18
+MAX_TEMPERATURE = 22
+
+def notify_to_line(message):
+    payload = {
+        "message": message,
+        "notificationDisabled": False,
+    }
+    print(payload)
+    res = requests.post(url, headers = headers, params = payload)
+    print(res)
 
 try:
-    while True:
-        result = instance.read()
-        if result.is_valid():
-            print("Last valid input: " + str(datetime.datetime.now()))
-            temperature = result.temperature
-            humidity = result.humidity
-            print("Temperature: %-3.1f C" % temperature)
-            print("Humidity: %-3.1f %%" % humidity)
-            # 温度が18℃未満なら「寒い」22℃以上なら「暑い」と通知する
-            print(temperature)
-            print(humidity)
-            if False: #temperature > 22:
-                print("暑いよーーー")
-                message = f"暑いよーーー><\n温度: {temperature} ℃\n湿度: {humidity} %"
-                print(res)
-            elif True: #temperature < 22:
-                print("寒いよーーー><")
-                message = f"寒いよーーー><\n温度: {temperature} ℃\n湿度: {humidity} %"
-
-            payload = {
-                "message": message,
-                "notificationDisabled": False
-            }
-            res = requests.post(url, headers = headers, params = payload)
+    result = instance.read()
+    if result.is_valid():
+        print("Last valid input: " + str(datetime.datetime.now()))
+        temperature = result.temperature
+        humidity = result.humidity
+        print("Temperature: %-3.1f C" % temperature)
+        print("Humidity: %-3.1f %%" % humidity)
+        print(temperature)
+        print(humidity)
+        if temperature > MAX_TEMPERATURE:
+            print("暑いよーーー")
+            message = f"暑いよーーー><\n温度: {temperature} ℃\n湿度: {humidity} %"
+            notify_to_line(message)
             print(res)
-
-        time.sleep(6)
+        elif temperature < MIN_TEMPERATURE:
+            print("寒いよーーー><")
+            message = f"寒いよーーー><\n温度: {temperature} ℃\n湿度: {humidity} %"
+            notify_to_line(message)
 
 except KeyboardInterrupt:
     print("Cleanup")
